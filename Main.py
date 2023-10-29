98ba2c3d6e0ae1db2411cfc47fe06bbbd6fdb2da
JohnyWest <bbassed@gmail.com>
2023-10-30 01:35:40 +0500

    add Date/Time and json file load

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
-import json
 import datetime
+import json
 
+#Получаем текущие дату/время
+def get_current_datetime():
+    return datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+
+#Загрузка заметок через json
+def load_notes():
+    try:
+        with open("notes.json", "r") as file:
+            notes = json.load(file)
+        return notes
+    except FileNotFoundError:
+        return[]
