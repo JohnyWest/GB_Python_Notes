e963c1181df33d20be9b04e478aae0db999a73c1
JohnyWest <bbassed@gmail.com>
2023-11-03 16:23:37 +0500

    Add Show notes

--- Main.py
+++ Main.py
@@ -24,16 +24,30 @@ def create_note():
         "title":title,
         "body":body,
         "created_at":get_current_datetime(),
-        "updated_at":get_current_datetime(),
+        "updated_at":get_current_datetime()
     }
     notes.append(note)
-    #save_notes
+    save_notes (notes)
     print("Заметка создана")
 
 #Сохранние заметки
 def save_notes(notes):
     with open("notes.json", "w") as file:
         json.dump(notes, file, indent=4)
+
+#ВЫвод всех заметок
+def show_notes():
+    notes = load_notes()
+    if notes:
+        for note in notes:
+            print("ID:", note["id"])
+            print("Заголовок:", note["title"])
+            print("Текст:", note["body"])
+            print("Дата создания:", note["created_at"])
+            print("Дата изменения:", note["updated_at"])
+            print("____________________________________")
+    else:
+        print("Заметок пока нет.")
 
 #Основной метод
 def main():
@@ -44,11 +58,11 @@ def main():
 
         choice = input("Выберите действие: ")
         if choice == "1":
-            create_note()
             print("Вы выбрали создать заметку")
+            create_note()
         elif choice == "2":
-            load_notes()
             print("Вы выбрали загрузить заметку")
+            load_notes()
 #TO DO: Тут короче подумать как лучше сделать методы
         break
     else:
